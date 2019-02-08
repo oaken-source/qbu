8105fdd7332c0a818ff5f7bae2364b98f6565a73
Andreas Grapentin <andreas@grapentin.org>
2019-02-08 22:17:46 +0100

    added license

--- qbu.sh
+++ qbu.sh
@@ -1,3 +1,21 @@
+###############################################################################
+#       qbu -- (q)ueued (bu)ild manager for libretools                        #
+#                                                                             #
+#     Copyright (C) 2019  Andreas Grapentin                                   #
+#                                                                             #
+#     This program is free software: you can redistribute it and/or modify    #
+#     it under the terms of the GNU General Public License as published by    #
+#     the Free Software Foundation, either version 3 of the License, or       #
+#     (at your option) any later version.                                     #
+#                                                                             #
+#     This program is distributed in the hope that it will be useful,         #
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#     GNU General Public License for more details.                            #
+#                                                                             #
+#     You should have received a copy of the GNU General Public License       #
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.   #
+###############################################################################
 #!/bin/bash
 
 set -euo pipefail
@@ -157,8 +175,3 @@ case "${1:-l}" in
   x) run_build "${@:2}" ;;
   *) error "$1: unrecognized command" && exit $EXIT_FAILURE ;;
 esac
-
-
-
-
-
