a1870321edd4e9135a8faefbe819b9288f97750a
Andreas Grapentin <andreas@grapentin.org>
2019-02-08 22:44:08 +0100

    fixed some code smells, added beginnings of a readme

---
+++ README.rst
@@ -0,0 +1,9 @@
+
+(q)ueue (bu)ild manager
+=======================
+
+this is a simple convenience tool for batched builds using parabola libretools
+for multiple arches. It manages a build queue, retains build logs and
+implements notifications for failed or successful builds.
+
+TODO.
--- qbu.sh
+++ qbu.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ###############################################################################
 #       qbu -- (q)ueued (bu)ild manager for libretools                        #
 #                                                                             #
@@ -16,7 +17,6 @@
 #     You should have received a copy of the GNU General Public License       #
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.   #
 ###############################################################################
-#!/bin/bash
 
 set -euo pipefail
 [ -z "${DEBUG:-}" ] || set -x
@@ -27,24 +27,28 @@ set -euo pipefail
 list_builds() {
   local format="%-3s   %-40s   %-6s   %-8s   %-14s\n"
   printf "$format" "ID" "Package" "Arch" "State" "Times(r/u/s)"
-  local snap=$(tsp | tail -n+2)
 
-  local running=$(echo "$snap" | grep ' running ')
+  local snap
+  snap=$(tsp | tail -n+2)
+
+  local running queued failed finished
+
+  running=$(echo "$snap" | grep ' running ')
   [ -n "$running" ] && while read -r line; do
     printf "$format" $(echo "$line" | awk '{print $1, $(NF-1), $(NF), $2}') ""
   done <<< "$running"
 
-  local queued=$(echo "$snap" | grep ' queued ')
+  queued=$(echo "$snap" | grep ' queued ')
   [ -n "$queued" ] && while read -r line; do
     printf "$format" $(echo "$line" | awk '{print $1, $(NF-1), $(NF), $2}') ""
   done <<< "$queued"
 
-  local failed=$(echo "$snap" | grep ' finished ' | awk '$4 != "0" {print $0}')
+  failed=$(echo "$snap" | grep ' finished ' | awk '$4 != "0" {print $0}')
   [ -n "$failed" ] && while read -r line; do
     printf "$format" $(echo "$line" | awk '{print $1, $(NF-1), $(NF), "failed", $5}')
   done <<< "$failed"
 
-  local finished=$(echo "$snap" | grep ' finished ' | awk '$4 == "0" {print $0}')
+  finished=$(echo "$snap" | grep ' finished ' | awk '$4 == "0" {print $0}')
   [ -n "$finished" ] && while read -r line; do
     printf "$format" $(echo "$line" | awk '{print $1, $(NF-1), $(NF), $2, $5}')
   done <<< "$finished"
@@ -56,10 +60,12 @@ list_builds() {
 }
 
 enqueue_builds() {
-  [ -f PKGBUILD ] || (error "missing PKGBUILD" && return $EXIT_FAILURE)
-  local pkglist=$(makepkg --packagelist 2>&1) || return $EXIT_FAILURE
-  local pkgname=( $(echo "$pkglist" | rev | cut -d'-' -f2- | rev) )
-  [ -n "$pkgname" ] || (error "malformed PKGBUILD" && return $EXIT_FAILURE)
+  local pkglist pkgname running
+
+  [ -f PKGBUILD ] || (error "missing PKGBUILD" && return "$EXIT_FAILURE")
+  pkglist=$(makepkg --packagelist 2>&1) || return "$EXIT_FAILURE"
+  pkgname=( $(echo "$pkglist" | rev | cut -d'-' -f2- | rev) )
+  [ -n "${pkgname[0]}" ] || (error "malformed PKGBUILD" && return "$EXIT_FAILURE")
 
   local arches=()
   if [ $# -gt 0 ]; then
@@ -67,7 +73,7 @@ enqueue_builds() {
   else
     arches+=( $(echo "$pkglist" | rev | cut -d'-' -f1 | rev | cut -d'.' -f1 \
         | sed "s@any@$(uname -m)@" | sort -r | uniq) )
-    [ -n "${arches[0]}" ] || (error "malformed PKGBUILD" && return $EXIT_FAILURE)
+    [ -n "${arches[0]}" ] || (error "malformed PKGBUILD" && return "$EXIT_FAILURE")
   fi
 
   load_conf libretools.conf ARCHES
@@ -76,13 +82,13 @@ enqueue_builds() {
 
   local a
   for a in "${arches[@]}"; do
-    local running=$(tsp | grep ' running ' | grep -q " x $pkgname $a")
-    [ -z "$running" ] || warning "$pkgname-$a: build is already running"
+    running=$(tsp | grep ' running ' | grep -q " x ${pkgname[0]} $a")
+    [ -z "$running" ] || warning "${pkgname[0]}-$a: build is already running"
     local id
-    for id in $(tsp | grep -v ' running ' | grep " x $pkgname $a" | awk '{print $1}'); do
-      tsp -r $id
+    for id in $(tsp | grep -v ' running ' | grep " x ${pkgname[0]} $a" | awk '{print $1}'); do
+      tsp -r "$id"
     done
-    tsp "$0" x $pkgname $a
+    tsp "$0" x "${pkgname[0]}" "$a"
   done
 }
 
@@ -94,17 +100,17 @@ kill_build() {
 clear_builds() {
   if [ $# -gt 0 ]; then
     local id
-    for id in "$@"; do tsp -r $id; done
+    for id in "$@"; do tsp -r "$id"; done
   else
     for id in $($0 | awk '$4 == "finished" {print $1}'); do
-      tsp -r $id
+      tsp -r "$id"
     done
   fi
 }
 
 show_build() {
   if [ $# -gt 0 ]; then
-    tsp -c $1
+    tsp -c "$1"
   else
     while tsp | grep -q ' running '; do tsp -c; done
   fi
@@ -118,7 +124,7 @@ notify() {
 
 build_error() {
   local a="${1//_/\\_}"
-  notify -c error "*[Q$($0 | tail -n1 | awk '{print $4}' | tr -d ',')]* $a" -h string:document:$2
+  notify -c error "*[Q$($0 | tail -n1 | awk '{print $4}' | tr -d ',')]* $a" -h "string:document:$2"
 }
 
 build_success() {
@@ -128,50 +134,64 @@ build_success() {
 
 prepare_chroot() {
   # clean the chroot
-  sudo librechroot -A $1 -n qbu-$1 clean-pkgs && _clean=yes || _clean=no
+  sudo librechroot -A "$1" -n "qbu-$1" clean-pkgs && _clean=yes || _clean=no
   if [ "x$_clean" == "xno" ]; then
-    sudo librechroot -A $1 -n qbu-$1 delete
-    sudo librechroot -A $1 -n qbu-$1 -l root delete
-    sudo librechroot -A $1 -n qbu-$1 make
-    sudo librechroot -A $1 -n qbu-$1 clean-pkgs || return
+    sudo librechroot -A "$1" -n "qbu-$1" delete
+    sudo librechroot -A "$1" -n "qbu-$1" -l root delete
+    sudo librechroot -A "$1" -n "qbu-$1" make
+    sudo librechroot -A "$1" -n "qbu-$1" clean-pkgs || return
   fi
 
   # update the chroot
-  sudo librechroot -A $1 -n qbu-$1 update && _updated=yes || _updated=no
+  sudo librechroot -A "$1" -n "qbu-$1" update
   if [ "x$_clean" == "xno" ]; then
-    sudo librechroot -A $1 -n qbu-$1 delete
-    sudo librechroot -A $1 -n qbu-$1 -l delete
-    sudo librechroot -A $1 -n qbu-$1 make
-    sudo librechroot -A $1 -n qbu-$1 update || return
+    sudo librechroot -A "$1" -n "qbu-$1" delete
+    sudo librechroot -A "$1" -n "qbu-$1" -l delete
+    sudo librechroot -A "$1" -n "qbu-$1" make
+    sudo librechroot -A "$1" -n "qbu-$1" update || return
   fi
 
   # clean the chroot pkg cache
-  sudo librechroot -A $1 -n qbu-$1 run \
+  sudo librechroot -A "$1" -n "qbu-$1" run \
     find /var/cache/pacman/pkg -type f -delete
 }
 
 run_build() {
-  [ -f PKGBUILD ] || (error "missing PKGBUILD" && return $EXIT_FAILURE)
+  [ -f PKGBUILD ] || (error "missing PKGBUILD" && return "$EXIT_FAILURE")
 
   out="$(mktemp)"
-  prepare_chroot $2 2>&1 | tee "$out" || build_error "broken qbu-$2" "$out"
+  prepare_chroot "$2" 2>&1 | tee "$out" || build_error "broken qbu-$2" "$out"
   rm "$out"
   out="$(mktemp)"
   set +e
-  sudo libremakepkg -n qbu-$2 2>&1 | tee "$out"
+  sudo libremakepkg -n "qbu-$2" 2>&1 | tee "$out"
   res=$?
   set -e
-  [ $res -eq 0 ] && build_success "$1-$2" || build_error "$1-$2" "$out"
+
+  if [ $res -eq 0 ]; then
+    build_success "$1-$2"
+  else
+    build_error "$1-$2" "$out"
+  fi
+
   rm "$out"
   return $res
 }
 
 case "${1:-l}" in
-  l) list_builds "${@:2}" ;;
-  q) enqueue_builds "${@:2}" ;;
-  k) kill_build "${@:2}" ;;
-  r) clear_builds "${@:2}" ;;
-  c) show_build "${@:2}" ;;
-  x) run_build "${@:2}" ;;
-  *) error "$1: unrecognized command" && exit $EXIT_FAILURE ;;
+  l)
+    list_builds "${@:2}" ;;
+  q)
+    enqueue_builds "${@:2}" ;;
+  k)
+    kill_build "${@:2}" ;;
+  r)
+    clear_builds "${@:2}" ;;
+  c)
+    show_build "${@:2}" ;;
+  x)
+    run_build "${@:2}" ;;
+  *)
+    error "$1: unrecognized command"
+    exit "$EXIT_FAILURE" ;;
 esac
